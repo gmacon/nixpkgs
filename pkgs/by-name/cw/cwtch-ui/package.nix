3c0ba00f61b3dafa2cd0d42a780445bfcfc0d169
George Macon <george@themacons.net>
2024-06-16 18:09:46 -0400

    cwtch-ui: init at 1.14.7

---
+++ pkgs/by-name/cw/cwtch-ui/package.nix
@@ -0,0 +1,55 @@
+{ cwtch
+, fetchgit
+, flutter
+, gnome
+, lib
+, tor
+}:
+let
+  runtimeBinDependencies = [ tor gnome.zenity ];
+in
+flutter.buildFlutterApplication rec {
+  pname = "cwtch-ui";
+  version = "1.14.7";
+  src = fetchgit {
+    url = "https://git.openprivacy.ca/cwtch.im/cwtch-ui";
+    rev = "v${version}";
+    hash = "sha256-c02s8YFrLwIpvLVMM2d7Ynk02ibIgZmRKOI+mkrttLk=";
+  };
+
+  patches = [
+    ./exhaustive-match.patch
+  ];
+
+  pubspecLock = lib.importJSON ./pubspec.json;
+  gitHashes = {
+    flutter_gherkin = "sha256-NshzlM21x7jSFjP+M0N4S7aV3BcORkZPvzNDwJxuVSA=";
+  };
+  vendorHash = "sha256-QRmhprObwn9jAbX+GQLDtHUPCQ6WX0AgTjFqTwCceTc=";
+  flutterBuildFlags = [
+    "--dart-define"
+    "BUILD_VER=${version}"
+    "--dart-define"
+    "BUILD_DATE=$(date +%G-%m-%d-%H-%M --date=@$SOURCE_DATE_EPOCH)"
+  ];
+
+  # These things are added to LD_LIBRARY_PATH, but not PATH
+  runtimeDependencies = [ cwtch ];
+
+  extraWrapProgramArgs = "--prefix PATH : ${lib.makeBinPath runtimeBinDependencies}";
+
+  postInstall = ''
+    mkdir -p $out/share/applications
+    sed "s|PREFIX|$out|" linux/cwtch.template.desktop >$out/share/applications/cwtch.desktop
+  '';
+
+  meta = with lib; {
+    description = "A decentralized, privacy-preserving, multi-party messaging app";
+    homePage = "https://cwtch.im/";
+    changelog = "https://cwtch.im/changelog/";
+    license = licenses.mit;
+    mainProgram = "cwtch";
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ gmacon ];
+  };
+}
